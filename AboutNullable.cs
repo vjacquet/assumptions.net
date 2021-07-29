3151e1cb3e7b4c92e88a5d7c28810d88239608a0
Vincent JACQUET <vjacquet@flowgroup.fr>
2021-07-29 19:07:46 +0200

    Add more facts about nullables.

--- AboutNullable.cs
+++ AboutNullable.cs
@@ -15,5 +15,24 @@ namespace NetCore.Assumptions
 
             Assert.Equal(0, counter.Tally);
         }
+
+        [Fact]
+        public void NullIsAbsorbantForArithmetics()
+        {
+            int i = 1;
+            int? j = null;
+            var result = i + j;
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void FallbackHasLessPrecedenceThanAdd()
+        {
+            int a = 10;
+            int b = 20;
+            int? c = null;
+            var result = a + c ?? b;
+            Assert.Equal(20, result);
+        }
     }
 }
