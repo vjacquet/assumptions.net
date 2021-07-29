596b2168e67479b67ada7df9079bd0c71d03dda6
Vincent JACQUET <vjacquet@flowgroup.fr>
2021-07-29 19:06:47 +0200

    Add AboutQueryable

---
+++ AboutQueryable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace NetCore.Assumptions
+{
+    public class AboutQueryable
+    {
+        [Fact]
+        public void AssumeQueryableIsPropagated()
+        {
+            int[] array = new[] { 1, 2, 3, 4, 5, 6 };
+            var queryable = array.AsQueryable();
+            var query = from i in queryable
+                        where i % 2 == 0
+                        select i;
+
+            Assert.IsAssignableFrom<IEnumerable<int>>(query);
+            Assert.IsAssignableFrom<IQueryable<int>>(query);
+        }
+    }
+}
