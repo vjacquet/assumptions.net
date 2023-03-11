ce90ea48045f58ae830e289569f65bfbdc84136a
Vincent JACQUET <vjacquet@flowgroup.fr>
2023-03-11 16:29:35 +0100

    feat: added for benchmarks

---
+++ Benchmarks/for_vs_foreach_vs_ForEach.cs
@@ -0,0 +1,67 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using System.Runtime.InteropServices;
+
+namespace Benchmarks
+{
+	[SimpleJob(RuntimeMoniker.Net60)]
+	[SimpleJob(RuntimeMoniker.Net70)]
+	[RPlotExporter]
+	public class for_vs_foreach_vs_ForEach
+	{
+		private List<int>? _items;
+
+		[GlobalSetup]
+		public void Setup()
+		{
+			var random = new Random(420);
+			_items = new List<int>(Size);
+			_items.AddRange(Enumerable.Range(1, Size).Select(_ => random.Next()));
+		}
+
+		[Params(100, 100_000)] public int Size { get; set; }
+
+		[Benchmark(Baseline = true)]
+		public void Foreach()
+		{
+			foreach (var item in _items)
+			{
+				Noop(item);
+			}
+		}
+
+		[Benchmark]
+		public void For()
+		{
+			for (var i = 0; i < _items.Count; i++)
+			{
+				Noop(_items[i]);
+			}
+		}
+
+		[Benchmark]
+		public void ForeachLinq()
+		{
+			_items.ForEach(Noop);
+		}
+
+		[Benchmark]
+		public void ParallelForeachLinq()
+		{
+			Parallel.ForEach(_items, Noop);
+		}
+
+		[Benchmark]
+		public void Span()
+		{
+			foreach (var item in CollectionsMarshal.AsSpan(_items))
+			{
+				Noop(item);
+			}
+		}
+
+		private static void Noop(int value)
+		{
+		}
+	}
+}
---
+++ Benchmarks/for_vs_forrange.cs
@@ -0,0 +1,83 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Runtime.CompilerServices;
+using Benchmarks.Internals;
+using BenchmarkDotNet.Jobs;
+
+namespace Benchmarks
+{
+	[SimpleJob(RuntimeMoniker.Net60)]
+	[SimpleJob(RuntimeMoniker.Net70)]
+	[RPlotExporter]
+	public class for_vs_forrange
+	{
+		[Params(10, 100, 1000, 10_000)]
+		public int Size { get; set; }
+
+		[Benchmark(Baseline = true)]
+		public void ForLoop()
+		{
+			for (int i = 0; i < Size; i++)
+			{
+				DoSomething(i);
+			}
+		}
+
+
+		[Benchmark]
+		public void ForeachRangeLoop()
+		{
+			foreach (int i in 0..Size)
+			{
+				DoSomething(i);
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.NoOptimization)]
+		private static void DoSomething(int _)
+		{
+		}
+	}
+}
+
+namespace Benchmarks.Internals
+{
+	internal static class RangeExtensions
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Enumerator GetEnumerator(this Range range)
+		{
+			return new Enumerator(range);
+		}
+
+		public ref struct Enumerator
+		{
+			private int _current;
+			private int _start;
+			private readonly int _end;
+			private readonly int _stride;
+
+			public Enumerator(Range range)
+			{
+				_current = default;
+				_start = range.Start.GetOffset(int.MaxValue);
+				_end = range.End.GetOffset(int.MaxValue);
+				_stride = _start < _end ? +1 : -1;
+			}
+
+			public int Current => _current;
+
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			public bool MoveNext()
+			{
+				if (_start != _end)
+				{
+					_current = _start;
+					_start += _stride;
+					return _current != _end;
+				}
+				return false;
+			}
+		}
+	}
+
+}
